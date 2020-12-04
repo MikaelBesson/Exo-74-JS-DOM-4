bd67949d0b5fa94fae58414aa8ee2e7083afa36b
BessonMikael <besson.mikael04@gmail.com>
2020-12-04 12:42:20 +0100

    exo 74

--- exo 1/exercice1.js
+++ exo 1/exercice1.js
@@ -1,6 +1,21 @@
 //Votre code ci dessous
 
 
+let cat =document.getElementById("cat-pic");
+
+function miaou(){
+    document.getElementById("cat-chat").innerHTML = "Me touche pas Humain !!!";
+}
+
+
+
+cat.addEventListener("mouseenter", miaou);
+
+
+
+
+
+
 
 
 
